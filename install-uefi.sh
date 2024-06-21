e22cf2e19a93facf8a78a05bbe1bb1705e13144b
rahmathali60 <81515092+rahmathali60@users.noreply.github.com>
2024-06-21 16:28:38 +0530

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -16,7 +16,7 @@ echo root:0985 | chpasswd
 # You can remove the tlp package if you are installing on a desktop or vm
 pacman -Syy
 pacman -S grub efibootmgr bluez bluez-utils networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils nfs-utils inetutils dnsutils alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion acpi acpi_call bridge-utils dnsmasq ipset sof-firmware nss-mdns acpid os-prober ntfs-3g 
-pacman -S 
+
 
 # pacman -S --noconfirm xf86-video-amdgpu
 #pacman -S --noconfirm nvidia-lts nvidia-utils nvidia-settings
